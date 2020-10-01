c1a483bf4cf5500edd34a18eb5c38982b1cf0bb7
Hafiz <hafiz.nr5758@gmail.com>
2020-10-01 09:19:30 +0700

    update: hello-kt

--- hello-kt.kt
+++ hello-kt.kt
@@ -1,8 +1,15 @@
 fun main() {
     println("Hello World")
     showYourName("hafiz")
+    showYourNameInLoop(0..5, "hafiz")
 }
 
-fun showYourName(message: String){
-    print(message)
+fun showYourName(name: String){
+    print(name)
+}
+
+fun showYourNameInLoop(size: IntRange, name: String){
+    for(i in size){
+        println(name)
+    }
 }
